934c2f6890fad3f4dea052fc59ec832346a2b0de
webdude <dimo.petrof@gmail.com>
2016-11-28 22:15:39 +0200

    start an empty code first app from scratch

---
+++ code-first/src/main/java/eu/webdude/Startup.java
@@ -0,0 +1,13 @@
+package eu.webdude;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Startup implements CommandLineRunner {
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("Started");
+	}
+}
