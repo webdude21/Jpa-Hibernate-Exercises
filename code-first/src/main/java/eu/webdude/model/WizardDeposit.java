60bc15e43fa57aa5e95f9752cdddc38e99d14ab1
webdude <dimo.petrof@gmail.com>
2016-12-03 09:41:26 +0200

    add age validation

--- code-first/src/main/java/eu/webdude/model/WizardDeposit.java
+++ code-first/src/main/java/eu/webdude/model/WizardDeposit.java
@@ -87,6 +87,10 @@ public class WizardDeposit {
 	}
 
 	public void setAge(int age) {
+		if (age < 0 || age > 120) {
+			throw new IllegalArgumentException("Age must be between 0 and 120 years");
+		}
+
 		this.age = age;
 	}
 
