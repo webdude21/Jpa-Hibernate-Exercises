5403b8d8d5e3415cdba28335a82d58558e5b9c93
webdude <dimo.petrof@gmail.com>
2016-12-11 20:47:03 +0200

    demonstrating the advantage of queryDsl over JPQL

--- criteria-api-specs/src/main/java/eu/webdude/Startup.java
+++ criteria-api-specs/src/main/java/eu/webdude/Startup.java
@@ -3,31 +3,52 @@ package eu.webdude;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import eu.webdude.model.QEmployee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Component
 public class Startup implements CommandLineRunner {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	private static final String AVERAGE_SALARY_TEXT = "Average salary of employees is %f%n";
+
+	private final JPAQueryFactory query;
+
+	private final EntityManager entityManager;
 
 	private QEmployee employee;
 
-	Startup() {
+	@Autowired
+	Startup(EntityManager entityManager) {
+		this.entityManager = entityManager;
 		this.employee = QEmployee.employee;
+		this.query = new JPAQueryFactory(entityManager);
+	}
+
+	private double getAverageSalaryWithJpdl(String whereClause) {
+		return (double) entityManager.createQuery("select avg(e.salary) from Employee as e" + whereClause).getSingleResult();
+	}
+
+	private Double getAverageSalaryWithQueryDsl(BooleanExpression... predicates) {
+		return query
+			.select(employee.salary.avg())
+			.from(employee)
+			.where(predicates)
+			.fetchOne();
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
+		System.out.printf(AVERAGE_SALARY_TEXT, getAverageSalaryWithJpdl(" where e.manager IS NULL"));
+		System.out.printf(AVERAGE_SALARY_TEXT, getAverageSalaryWithQueryDsl(employee.manager.isNull()));
+		getAverageSalaryWithQueryDsl();
 		countBy(employee.lastName.endsWithIgnoreCase("on"), employee.firstName.startsWithIgnoreCase("T"));
 	}
+
 	private void countBy(BooleanExpression... predicates) {
-		JPAQueryFactory query = new JPAQueryFactory(entityManager);
-		long resultEmplCount = query.selectFrom(employee)
+		long resultEmplCount = query.selectFrom(employee).select(employee.salary)
 			.where(predicates)
 			.fetchCount();
 
