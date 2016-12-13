f9514cac5e51e421b6ec61f5c3335b6c47558bae
webdude <dimo.petrof@gmail.com>
2016-12-13 22:03:55 +0200

    solved one

--- criteria-api-specs/src/main/java/eu/webdude/Startup.java
+++ criteria-api-specs/src/main/java/eu/webdude/Startup.java
@@ -1,6 +1,7 @@
 package eu.webdude;
 
 import eu.webdude.queries.ComparingJQPLWithQueryDSL;
+import eu.webdude.queries.QueriesExercise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,13 +11,17 @@ public class Startup implements CommandLineRunner {
 
 	private ComparingJQPLWithQueryDSL comparingJQPLWithQueryDSL;
 
+	private QueriesExercise queriesExercise;
+
 	@Autowired
-	Startup(ComparingJQPLWithQueryDSL comparingJQPLWithQueryDSL) {
+	Startup(ComparingJQPLWithQueryDSL comparingJQPLWithQueryDSL, QueriesExercise queriesExercise) {
 		this.comparingJQPLWithQueryDSL = comparingJQPLWithQueryDSL;
+		this.queriesExercise = queriesExercise;
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
 		comparingJQPLWithQueryDSL.testQuery();
+		queriesExercise.runQueries();
 	}
 }
--- criteria-api-specs/src/main/java/eu/webdude/queries/QueriesExercise.java
+++ criteria-api-specs/src/main/java/eu/webdude/queries/QueriesExercise.java
@@ -1,13 +1,13 @@
 package eu.webdude.queries;
 
-import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import eu.webdude.model.QEmployee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
-import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class QueriesExercise {
@@ -25,8 +25,8 @@ public class QueriesExercise {
 		this.query = new JPAQueryFactory(entityManager);
 	}
 
-	void runQueries() {
-
+	public void runQueries() {
+		salaryOfThoseTakingTheMinimumSalary();
 	}
 
 	/**
@@ -34,10 +34,15 @@ public class QueriesExercise {
 	 * Use a nested SELECT statement.
 	 */
 	void salaryOfThoseTakingTheMinimumSalary() {
-		JPAQuery<BigDecimal> result = query
-			.select(employee.salary.min())
+		List<Tuple> result = query
+			.select(employee.firstName, employee.lastName)
 			.from(employee)
-			.fetchAll();
+			.where(employee.salary.eq(query.select(employee.salary.min()).from(employee).fetchOne()))
+			.fetch();
+
+		result.forEach(res -> System.out.printf("Name: %s %s%n",
+			res.get(employee.firstName),
+			res.get(employee.lastName)));
 	}
 
 }
